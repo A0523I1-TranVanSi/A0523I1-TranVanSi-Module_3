f06fe31a0948ac3565ce3bedbc97bb019bf3dd90
A0523I1-TranVanSi <sitran.120503@gmail.com>
2023-10-27 22:45:14 +0700

    ss1

---
+++ ss1_CSDL_QH/exercise/student_management.sql
@@ -0,0 +1,11 @@
+use student_management ; 
+create table class(
+	id int , 
+    name varchar(20) 
+) ; 
+create table teacher(
+	id int ,
+    name varchar(20) , 
+    age char(2),
+    country varchar(20) 
+);
